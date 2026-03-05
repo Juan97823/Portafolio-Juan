72d854f424671b6e521c37c53c5a9ebdf1afd4ae
Juan97823 <Juanesab423@gmail.com>
2026-03-04 19:53:47 -0500

    Update

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Portafolio cargado correctamente");
+// Scroll animation
+const sections = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", () => {
+  sections.forEach(section => {
+    const position = section.getBoundingClientRect().top;
+    if (position < window.innerHeight - 100) {
+      section.classList.add("visible");
+    }
+  });
+});
